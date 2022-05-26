be0078c971a9da56fe303a6a5058c25143a6dca5
abdalla.jama <abdallajama201@gmail.com>
2022-05-26 19:36:28 -0400

    All functionality complete as well as README and LICENSE

---
+++ README.md
@@ -0,0 +1,25 @@
+# 03 JavaScript Challenge: Password Generator
+ 
+## Description
+ 
+This project is a single webpage that consists of a calendar that allows users to schedule their day
+
+Users can store events for the working hours of the day in the browsers and have them readily available throughout the day. This project can be used as a daily time management tool similar to a todo list, but with the added functions of scheduling tasks.
+ 
+## Usage
+ 
+The website is live on the open web and can be accessed at https://abdallajama201.github.io/Challenge-05-Third-Party-APIs/
+
+When a user opens the webpage the are presented with the current date and a block column of blocks. Each block represents a working hour of the day in chronological order. The current hour is marked red, while previous hours are gray and the future hours are green. A user can type their appointment into any block and save it by pressing the adjacent icon in that block. If the user refreshes the screen or returns later that appointment will be saved. 
+
+A photo of the mockup is available below.
+ 
+![mockup of website](assets/images/mock-up.png)
+  
+## Credits
+ 
+The original webpage were provided by [Trilogy Education Services](https://www.trilogyed.com/universities/) and the [School of Continuing Studies - University of Toronto](https://learn.utoronto.ca/)
+ 
+## License
+ 
+ This project is under the MIT License with language provided by the Open Source Initiative.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+// Variables for save and notify functions
+let isMessageClearing = false;
+let clearingMessageCode = 0;
+
+
 function init() {
     displayToday();
     colorSchedule();
@@ -7,32 +12,38 @@ function init() {
 }
 
 // Prints current day
+// Updates the time every hour
 function displayToday() {
-    let today = moment().format("dddd, MMMM Do YYYY");
-    $("#currentDay").text(today);
+    function updateDate() {
+        let today = moment().format("dddd, MMMM Do YYYY");
+        $("#currentDay").text(today);
+    }
+    setInterval(updateDate(),(1000*60*60));
 }
 
 // Fills the colors of the schedule.
 // Based on the time
 function colorSchedule() {
     let now = moment().format("H");
-    
+    now = parseInt(now);
     // Loops through all hours
     // Checks against now and sets color
     for(let i = 8; i <= 18; i++) {
-        let hour = $(`#${i}`);
+        let hour = $(`#${i}`).attr("id");
         let section = $(`#${i}`).children().eq(1);
-        if(hour > now) {
+        hour = parseInt(hour);
+        if(hour < now) {
             section.addClass("past");
         }else if(hour === now) {
-            section.addClass("present");
+            section.addClass("present");                               
         }else {
             section.addClass("future");
         }
     }
 }
 
-
+// Checks if there are previous saves
+// If not sets empty string for each hour
 function storageSetup() {
     for(let i = 8; i <= 18; i++) {
         if(localStorage.getItem(`hour ${i}`) === null) {
@@ -41,15 +52,19 @@ function storageSetup() {
     }
 }
 
+// Displays the content from local storage
+// Either the previous entries
+// Or empty strings from previous function
 function displayStorage() {
     for(let i = 8; i <= 18; i++) {
         let storage = localStorage.getItem(`hour ${i}`);
         $(`#${i}`).children().eq(1).val(storage);
     }
-
-
 }
 
+// Waits for click on save button
+// Checks if text area is empty
+// If not sends the tect area content to local storage
 function saveEvent() {
     $(".row").on("click", ".saveBtn", function(event){
         event.preventDefault();
@@ -58,12 +73,36 @@ function saveEvent() {
         let content = saveBtn.parent().children().eq(1).val();
         let index = saveBtn.parent().attr("id");
         localStorage.setItem(`hour ${index}`, content);
-
+        if(localStorage.getItem(`hour ${index}`)) {
+            notifySave();
+        }
     });
 }
 
-init();
+// Provides a message when saving to localStorage
+// Clears the message after three seconds
+// Extends the message if function called within time limit
+function notifySave() {
+    if(!isMessageClearing) {
+        let container = $(".container");
+        container.prepend("<p>Appointment added to localStorage ✅</p>");
+        container.children().eq(0).css("margin-left", "400px");
+        clearNotify();
+    } else {
+        isMessageClearing = false;
+        clearTimeout(clearingMessageCode);
+        clearNotify();
+    }
+}
 
-console.log($("#8").children().eq(0).text());
-let x = $("#8");
-// localStorage.setItem(`hour 13`, "gjhggjhgj");
+// Clears the message after local storage
+function clearNotify() {
+    let container = $(".container");
+    isMessageClearing = true;
+    clearingMessageCode = setTimeout(function() {
+        container.children().eq(0).remove();
+        isMessageClearing = false;
+    }, 3000);
+} 
+
+init();
